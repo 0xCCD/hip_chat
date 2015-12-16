c79131afa595da55e511180b9adccf95b844ad8a
Syed Humza Shah <humzashah@gmail.com>
2015-12-16 14:58:27 +0000

    replaced to_s by to_path for Pathname

--- lib/hip_chat.rb
+++ lib/hip_chat.rb
@@ -3,5 +3,5 @@ require "hip_chat/version"
 module HipChat
 end
 
-parent_folder = Pathname.new(__FILE__).parent.to_s
+parent_folder = Pathname.new(__FILE__).parent.to_path
 Dir[File.join(parent_folder, "hip_chat", "*.rb")].each { |path| require(path) }
